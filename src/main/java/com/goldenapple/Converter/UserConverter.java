bbfd55e0f12abf56f58d146f8530dcf27002139c
istzustand <goaway>
2017-11-24 12:45:10 +0100

    added rest facade

--- src/main/java/com/goldenapple/Converter/UserConverter.java
+++ src/main/java/com/goldenapple/Converter/UserConverter.java
@@ -6,12 +6,14 @@
 package com.goldenapple.Converter;
 
 import com.goldenapple.entity.UserEntity;
+import com.goldenapple.service.UserService;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,12 +22,14 @@ import javax.persistence.PersistenceContext;
  *
  * @author istzustand
  */
-@Named
+
 @FacesConverter(forClass = UserEntity.class, managed = true)
 public class UserConverter implements Converter {
 
     @PersistenceContext(unitName = "primary")
     private EntityManager em;
+    @Inject
+    UserService userService;
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object modelValue) {
@@ -42,18 +46,23 @@ public class UserConverter implements Converter {
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String submittedValue) {
+        System.out.println("submitted: " + Long.valueOf(submittedValue));
+        
         if (em == null) {
             System.out.println("em is nich");
         }
-        System.out.println("submitted: " + Long.valueOf(submittedValue));
-
+                if (userService == null) {
+            System.out.println("Service is nich");
+        }
+                
         if (submittedValue == null || submittedValue.isEmpty()) {
             System.out.println("submitted = nil");
             return null;
         }
 
         try {
-            return em.find(UserEntity.class, Long.valueOf(submittedValue));
+            //return em.find(UserEntity.class, Long.valueOf(submittedValue));
+            return userService.getById(Long.valueOf(submittedValue));
         } catch (NumberFormatException e) {
             throw new ConverterException(new FacesMessage(submittedValue + " is not a valid User ID"), e);
         }
---
+++ src/main/java/com/goldenapple/Facade/UserFacade.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.goldenapple.Facade;
+
+import com.goldenapple.entity.UserEntity;
+import com.goldenapple.service.UserService;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ *
+ * @author istzustand
+ */
+
+@Stateless
+@Path("user")
+public class UserFacade {
+    
+    @Inject
+    UserService userService;
+    
+    @GET
+    public List<UserEntity> getAll(){
+        return userService.getAllUsers();
+    }
+    
+    @GET
+    @Path("{id}")
+    public UserEntity getById(@PathParam("id") long id){
+        return userService.getById(id);
+    }
+    
+    @POST
+    public void newUser(UserEntity user){
+        userService.saveUser(user);
+    }
+    
+    @DELETE
+    @Path("{id}")
+    public void remUserById(@PathParam("id") long id){
+        UserEntity user = userService.getById(id);
+        userService.removeUser(user);
+    }
+    
+    
+}
